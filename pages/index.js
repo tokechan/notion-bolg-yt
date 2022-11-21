d2c98e4818637317512e5e0f066b16ca5b7e8d55
yuta tokeshi <toke@Tokes-MBA-M2.local>
2022-11-21 19:42:35 +0900

    done

--- pages/index.js
+++ pages/index.js
@@ -87,5 +87,14 @@ export default function Home({ posts }) {
   );
 }
 
-//SSGを追加
+//ISRを追加
+export const getStaticProps = async () => {
+  const database = await getDatabase(databaseId);
 
+  return {
+    props: {
+      posts: database,
+    },
+    revalidate: 1,
+  };
+};
